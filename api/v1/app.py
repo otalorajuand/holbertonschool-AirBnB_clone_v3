b2473e998ca001da487858314ed29eaf85592a9f
otalorajuand <otalorajuand@gmail.com>
2022-12-14 19:16:21 +0000

    Addition of task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module contains a flask app
-"""
+"""Module for Flask REST app"""
 from models import storage
 from api.v1.views import app_views
 import os
@@ -9,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
