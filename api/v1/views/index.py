e3aa5b69d6eff7bb36ce8c262ff0f1bddc3e1272
otalorajuand <otalorajuand@gmail.com>
2022-12-14 18:41:09 +0000

    Addition of task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module for app_views /status route"""
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/url', methods=['GET'])
-def status()
+def status():
+"""Returns the status of /status route"""
     return (jsonify({'status':'ok'}))
