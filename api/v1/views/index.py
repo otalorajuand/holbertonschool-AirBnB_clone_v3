ceec1633d2765832e231f983b200d619d7dd2980
otalorajuand <otalorajuand@gmail.com>
2022-12-14 19:21:34 +0000

    Addition of task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,13 @@
 """Module for app_views /status route"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
 
 @app_views.route('/url', methods=['GET'])
 def status():
