9250e0aab29d6f74f18313b76ccba3b16e1c74c0
Vozudo <jploperatobon@gmail.com>
2022-12-15 15:45:30 -0500

    adittion of task 1

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -82,8 +82,8 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_get(self):
         """Test the get method with none id"""
-        obj = storage.get(City,None)
-        self.assertEqual(obj,None)
+        obj = storage.get(City, None)
+        self.assertEqual(obj, None)
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_new(self):
